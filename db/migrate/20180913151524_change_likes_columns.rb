e7b98d0a200ef5dfc7fe70bf4e1cb6c5c2579712
EC2 Default User <ec2-user@ip-172-31-14-81.us-east-2.compute.internal>
2018-11-16 12:09:26 +0000

    event

--- db/migrate/20180913151524_change_likes_columns.rb
+++ db/migrate/20180913151524_change_likes_columns.rb
@@ -1,6 +1,6 @@
 class ChangeLikesColumns < ActiveRecord::Migration[5.2]
   def change
     add_column :likes, :post_id, :integer
-    remove_column :likes, :post, :integer
+    remove_column :likes, :post
   end
 end
